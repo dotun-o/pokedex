4cc3701022bf81ce2f4ff1ff4246e92fd4ad9725
Dotun Ogunsakin <email@dotun.me>
2017-07-10 18:58:32 -0400

    Style improvements to modal (centering, hr, footer bg-color).

--- scripts/main.es6.js
+++ scripts/main.es6.js
@@ -26,18 +26,26 @@ function renderTiles(payload) {
 
 function createModal(tile) {
     var data = `<div id="${"modal-" + tile.name}" class="modal fade" role="dialog">
-  <div class="modal-dialog">
+  <div class="modal-dialog text-center">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">${tile.name}</h4>
       </div>
       <div class="modal-body">
-        <div class="text-center"><img src="${tile.img}" alt="Thumbnail: ${tile.name}"></div>
-        <p>Type: ${tile.type}</p>
-        <p>Height: ${tile.height}</p>
-        <p>Weight: ${tile.weight}</p>
-        <p>Weaknesses: ${tile.weaknesses}</p>
+        <div><img src="${tile.img}" alt="Thumbnail: ${tile.name}"></div>
+        <hr>
+        <h4>Type</h4>
+        <p>${tile.type}</p>
+        <hr>
+        <h4>Height</h4>
+        <p>${tile.height}</p>
+        <hr>
+        <h4>Weight</h4>
+        <p>${tile.weight}</p>
+        <hr>
+        <h4>Weaknesses</h4>
+        <p>${tile.weaknesses}</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
